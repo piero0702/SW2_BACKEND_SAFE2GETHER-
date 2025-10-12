1c58796f9b82ad966fce29504b580b3cccb59f97
AlejandroC <alexcabero@hotmail.com>
2025-10-12 11:43:30 -0500

    modificacion temporal(solo un import) del auth_controller.py para la creacion de publicaciones asociado al usuario logeado

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Header, HTTPException
 from app.services.users_service import UsersService
 from app.models.user import LoginRequest, TokenResponse
 
